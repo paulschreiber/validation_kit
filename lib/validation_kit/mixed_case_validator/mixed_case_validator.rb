318eeab489b459b2f327df880684069cb33a0996
Paul Schreiber <paulschreiber@gmail.com>
2018-02-18 23:31:25 -0500

    Revert "fix i18n path for mixed case validation errors"
    This reverts commit ad456d8437459f062ebb3fcb6368e7ad2c2d295c.

--- lib/validation_kit/mixed_case_validator/mixed_case_validator.rb
+++ lib/validation_kit/mixed_case_validator/mixed_case_validator.rb
@@ -22,13 +22,13 @@ module ValidationKit
                          :default => nil) or options[:attribute_name] or attribute
 
       if error == ALL_CAPS
-       message = I18n.t("activerecord.errors.models.attributes.#{model_name.underscore}.#{attribute}.all_caps",
+       message = I18n.t("activerecord.errors.models.#{model_name.underscore}.attributes.#{attribute}.all_caps",
                    :item => item_name,
                    :default => [:"activerecord.errors.models.#{model_name.underscore}.all_caps",
                                 options[:all_caps],
                                 :'activerecord.errors.messages.all_caps'])
       elsif error == ALL_LOWERCASE
-        message = I18n.t("activerecord.errors.models.attributes.#{model_name.underscore}.#{attribute}.all_lowercase",
+        message = I18n.t("activerecord.errors.models.#{model_name.underscore}.attributes.#{attribute}.all_lowercase",
                     :item => item_name,
                     :default => [:"activerecord.errors.models.#{model_name.underscore}.all_lowercase",
                                  options[:all_lowercase],
